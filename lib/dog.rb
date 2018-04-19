3a7ca7eafcd5f17e718e820dd962eb7453b3f9bd
Trevor Seitz <trevinla@gmail.com>
2018-04-19 22:04:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,8 +72,8 @@ class Dog
     name = dog[:name]
     breed = dog[:breed]
     new_dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    binding.pry
-    
+    # binding.pry
+
     if !new_dog.empty?
       dog_data = new_dog[0]
       dog_details = [[:id, dog_data[0]], [:name, dog_data[1]], [:breed, dog_data[2]]].to_h
